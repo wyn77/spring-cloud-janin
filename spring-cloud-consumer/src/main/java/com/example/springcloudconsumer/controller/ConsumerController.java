daec61b90765daa5aa6c5617d359357136927f71
wyn <1224149409@qq.com>
2020-09-09 23:37:42 +0800

    wyn 整合redis

--- spring-cloud-consumer/src/main/java/com/example/springcloudconsumer/controller/ConsumerController.java
+++ spring-cloud-consumer/src/main/java/com/example/springcloudconsumer/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.example.springcloudconsumer.controller;
 
 import com.example.springcloudconsumer.feign.ProductFeign;
+import com.example.springcloudconsumer.utils.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +14,19 @@ public class ConsumerController
 
     @Autowired
     private ProductFeign productFeign;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @GetMapping("consumer")
     public String getConsumer(@RequestParam("name") String name)
     {
         return productFeign.getProduct(name);
+    }
+
+    @GetMapping("redis")
+    public String getRedis(@RequestParam("key") String key)
+    {
+        redisUtil.set("key", key);
+        return String.valueOf(redisUtil.get("key"));
     }
 }
