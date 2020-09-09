da0ed640b495f74fa0793fe7f50794b75ca285a0
wyn <1224149409@qq.com>
2020-09-09 17:35:17 +0800

    wyn 首次提交 创建父工程

---
+++ src/test/java/com/example/springcloudjanin/SpringCloudJaninApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springcloudjanin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudJaninApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
