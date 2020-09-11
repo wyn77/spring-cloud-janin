737c47bf5a2dcadfeb7ff4ddcb30050368ca1615
wyn <1224149409@qq.com>
2020-09-11 23:48:02 +0800

    wyn 解决无法maven打包问题

---
+++ spring-cloud-entity/src/main/java/com/example/springcloudentity/SpringCloudEntityApplication.java
@@ -0,0 +1,16 @@
+package com.example.springcloudentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringCloudEntityApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringCloudEntityApplication.class, args);
+    }
+
+}
